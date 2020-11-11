8d4c9d6711688af4b4b7fcab63fea39ae611a0b9
adisryn10 <imadeadisuryanugraha@gmail.com>
2020-11-11 11:32:11 +0700

    base cms, login page

--- src/main/java/mtf/project/security/WebSecurityConfig.java
+++ src/main/java/mtf/project/security/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/").permitAll()
+                .loginPage("/login").permitAll()
                 .and() 
                 .csrf().disable()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
